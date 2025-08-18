5f08bac0a7a66af7b7b25bf2d040a7ea1f2eb532
GiantbYTES <schakedamir@gmail.com>
2025-08-18 16:14:27 +0300

    init project

---
+++ ipa-project/src/Components/Map/Map.jsx
@@ -0,0 +1,11 @@
+import "./Map.css";
+
+export function Map() {
+    return (
+        <div className="Map">
+
+			<p>Map Component</p>
+
+        </div>
+    );
+}
---
+++ ipa-project/src/Components/User/User.tsx
@@ -0,0 +1,11 @@
+import "./User.css";
+
+export function User() {
+    return (
+        <div className="User">
+
+			<p>User Component</p>
+
+        </div>
+    );
+}
