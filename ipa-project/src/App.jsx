020f51cdfc4e3897a484c633031526f6cac1d4dd
Aviv Ben Ezra <avivbe95@gmail.com>
2025-08-20 16:12:55 +0300

    Adding

--- ipa-project/src/App.jsx
+++ ipa-project/src/App.jsx
@@ -1,11 +1,19 @@
 import "./App.css";
+
 import Home from "./Pages/Home";
+import Login from "./Pages/Login";
 import { MantineProvider } from "@mantine/core";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <MantineProvider>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </BrowserRouter>
     </MantineProvider>
   );
 }
